9a4ddd4058ca4436fa3e9836bc16bf41a0440866
Jared Mejia <emejia@ingenieria.digital>
2025-04-10 11:20:48 -0600

    feat: netlify

--- utils/database.util.js
+++ utils/database.util.js
@@ -1,26 +1,7 @@
-const { Sequelize, DataTypes } = require("sequelize");
-const dotenv = require("dotenv");
-
-dotenv.config({ path: "./config.env" });
+const { DataTypes, Sequelize } = require("sequelize");
+const { getSequelize } = require("./sequelize.utils");
 
 // Establish db connection
-const db = new Sequelize({
-  dialect: "postgres",
-  host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  database: process.env.DB,
-  logging: false,
-  dialectOptions:
-    process.env.NODE_ENV === "production"
-      ? {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        }
-      : {},
-});
+const db = getSequelize();
 
 module.exports = { db, DataTypes };
---
+++ utils/sequelize.utils.js
@@ -0,0 +1,28 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const { Sequelize } = require("sequelize");
+
+function getSequelize(params) {
+  return new Sequelize({
+    dialect: "postgres",
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB,
+    logging: false,
+    dialectOptions:
+      process.env.NODE_ENV === "production"
+        ? {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false,
+            },
+          }
+        : {},
+    ...params,
+  });
+}
+
+module.exports = { getSequelize };
