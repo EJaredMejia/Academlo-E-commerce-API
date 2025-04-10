3b1b16789284a9f0b0cedff01469316854373536
Jared Mejia <emejia@ingenieria.digital>
2025-04-10 14:30:10 -0600

    fix: serverless

--- .netlify/functions/api.js
+++ .netlify/functions/api.js
@@ -2,14 +2,11 @@ const serverless = require("serverless-http");
 const { app } = require("../../app");
 const { loadModels } = require("../../utils/load-models.util");
 
-module.exports.handler = async function () {
+module.exports.handler = async function (...args) {
   const db = await loadModels();
   try {
-    console.log(db);
-    return serverless(app);
+    return await serverless(app)(...args);
   } finally {
-    console.log("finally");
-    console.log(db);
     await db.connectionManager.close();
   }
 };
--- utils/load-models.util.js
+++ utils/load-models.util.js
@@ -2,8 +2,6 @@ const { db } = require("./database.util");
 const { initModels } = require("./../models/initModels");
 
 async function loadModels() {
-  console.log("loading models");
-  console.log(db);
   await db.authenticate();
 
   // Establish the relations between models
