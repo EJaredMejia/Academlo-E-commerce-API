394d001119047711f89072ab12dbcd288d832342
Jared Mejia <emejia@ingenieria.digital>
2025-04-10 14:14:46 -0600

    feat: logs for debuggin deploy

--- .netlify/functions/api.js
+++ .netlify/functions/api.js
@@ -5,8 +5,11 @@ const { loadModels } = require("../../utils/load-models.util");
 module.exports.handler = async function () {
   const db = await loadModels();
   try {
+    console.log(db);
     return serverless(app);
   } finally {
+    console.log("finally");
+    console.log(db);
     await db.connectionManager.close();
   }
 };
--- utils/load-models.util.js
+++ utils/load-models.util.js
@@ -2,6 +2,8 @@ const { db } = require("./database.util");
 const { initModels } = require("./../models/initModels");
 
 async function loadModels() {
+  console.log("loading models");
+  console.log(db);
   await db.authenticate();
 
   // Establish the relations between models
