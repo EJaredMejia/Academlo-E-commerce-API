993448145842da3f930d834f642455fcbdcfc33e
Jared Mejia <jaredmejia4440@gmail.com>
2026-02-02 11:41:35 -0600

    fix: sequelize serverless

--- src/utils/database.util.ts
+++ src/utils/database.util.ts
@@ -2,6 +2,7 @@ import { DataTypes, Sequelize } from "sequelize";
 import type { Dialect, Options } from "sequelize";
 import dotenv from "dotenv";
 import path from "path";
+import pg from "pg";
 
 // Ensure environment variables are loaded
 dotenv.config({ path: path.join(__dirname, "../../config.env") });
@@ -9,26 +10,39 @@ dotenv.config({ path: path.join(__dirname, "../../config.env") });
 let sequelizeInstance: Sequelize | null = null;
 
 export function getDb(): Sequelize {
-  if (!sequelizeInstance) {
-    const options: Options = {
-      host: process.env.DB_HOST || "localhost",
-      port: Number(process.env.DB_PORT) || 5432,
-      dialect: "postgres" as Dialect,
-      logging: false,
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      },
-    };
-
-    const database = process.env.DB || "test";
-    const username = process.env.DB_USERNAME || "postgres";
-    const password = process.env.DB_PASSWORD || "postgres";
-
-    sequelizeInstance = new Sequelize(database, username, password, options);
+  if (sequelizeInstance) {
+    return sequelizeInstance;
   }
+
+  sequelizeInstance = new Sequelize({
+    dialect: "postgres",
+    dialectModule: pg,
+    host: process.env.DB_HOST || "localhost",
+    username: process.env.DB_USERNAME || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    port: Number(process.env.DB_PORT) || 5432,
+    database: process.env.DB || "test",
+    logging: false,
+    dialectOptions:
+      process.env.NODE_ENV === "production"
+        ? {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false,
+            },
+          }
+        : {},
+    ...(process.env.SERVERLESS === "true"
+      ? {
+          pool: {
+            max: 2,
+            min: 0,
+            idle: 0,
+            acquire: 3000,
+          },
+        }
+      : {}),
+  });
 
   return sequelizeInstance;
 }
